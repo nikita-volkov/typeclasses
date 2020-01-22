7ea593a5f09f393f3e3d1c7184a48c0063367714
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2020-01-22 17:19:58 -0500

    Remove task from export list

--- src/Typeclasses/Classes/CommutativeMonoid.elm
+++ src/Typeclasses/Classes/CommutativeMonoid.elm
@@ -2,7 +2,7 @@ module Typeclasses.Classes.CommutativeMonoid exposing
     ( CommutativeMonoid
     , identityAndConcat, numberProduct, numberSum
     , map
-    , intProduct, intSum, setUnion, setDifference, cmd, sub, task
+    , intProduct, intSum, setUnion, setDifference, cmd, sub
     )
 
 {-| CommutativeMonoid typeclass definition and its instances for basic types.

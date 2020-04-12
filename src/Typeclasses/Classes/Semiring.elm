6e8a83bb42f48f832a6af4295fd61052448ee95e
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2020-04-12 17:34:51 -0400

    Expose ring instances in semiring

--- src/Typeclasses/Classes/Semiring.elm
+++ src/Typeclasses/Classes/Semiring.elm
@@ -1,4 +1,7 @@
-module Typeclasses.Classes.Semiring exposing (Semiring)
+module Typeclasses.Classes.Semiring exposing
+    ( Semiring
+    , exclusiveOrRing, numberRing, trivialRing
+    )
 
 {-| Semiring typeclass definition and its instances for basic types.
 
@@ -9,12 +12,63 @@ module Typeclasses.Classes.Semiring exposing (Semiring)
 
 -}
 
+import Typeclasses.Classes.AbelianGroup
+import Typeclasses.Classes.CommutativeMonoid
 import Typeclasses.Classes.Monoid
+import Typeclasses.Classes.Ring
 
 
 {-| Explicit typeclass which implements group operations for type `a`.
 -}
 type alias Semiring a =
-    { addition : Typeclasses.Classes.Monoid.Monoid a
+    { addition : Typeclasses.Classes.CommutativeMonoid.CommutativeMonoid a
     , multiplication : Typeclasses.Classes.Monoid.Monoid a
+    }
+
+
+{-| Construct real number ring
+-}
+numberRing : Semiring number
+numberRing =
+    let
+        ring =
+            Typeclasses.Classes.Ring.numberRing
+
+        (Typeclasses.Classes.AbelianGroup.AbelianGroup group) =
+            ring.addition
+    in
+    { addition = group.monoid
+    , multiplication = Typeclasses.Classes.Monoid.numberProduct
+    }
+
+
+{-| Construct trivial ring
+-}
+trivialRing : Semiring ()
+trivialRing =
+    let
+        ring =
+            Typeclasses.Classes.Ring.trivialRing
+
+        (Typeclasses.Classes.AbelianGroup.AbelianGroup group) =
+            ring.addition
+    in
+    { addition = group.monoid
+    , multiplication = Typeclasses.Classes.Monoid.unit
+    }
+
+
+{-| Construct exclusive all ring
+-}
+exclusiveOrRing : Semiring Bool
+exclusiveOrRing =
+    let
+        ring =
+            Typeclasses.Classes.Ring.exclusiveOrRing
+
+        (Typeclasses.Classes.AbelianGroup.AbelianGroup group) =
+            ring.addition
+    in
+    { addition = group.monoid
+    , multiplication = Typeclasses.Classes.Monoid.exclusiveOr
     }

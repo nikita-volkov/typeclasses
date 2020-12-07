e2328b6967ddd1d8848e7a4009a0a4af8a23a99a
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2020-12-07 13:35:39 -0500

    Add number Ring test

--- tests/RingTests.elm
+++ tests/RingTests.elm
@@ -97,4 +97,47 @@ suite =
                         && xXorYAllZ
                         == xAllZXorYAllZ
                     )
+        , Test.fuzz3
+            (Fuzz.intRange 1 10)
+            (Fuzz.intRange 1 10)
+            (Fuzz.intRange 1 10)
+            "tests numberRing all distributes over numberSum"
+          <|
+            \x y z ->
+                let
+                    (AbelianGroup.AbelianGroup groupAddition) =
+                        Ring.numberRing.addition
+
+                    xLeftMultiplyYplusZ =
+                        Ring.numberRing.multiplication.semigroup
+                            x
+                            (groupAddition.monoid.semigroup y z)
+
+                    yLeftMultiplyX =
+                        Ring.numberRing.multiplication.semigroup x y
+
+                    zLeftMultiplyX =
+                        Ring.numberRing.multiplication.semigroup x z
+
+                    yLeftMultiplyXPlusZLeftMultiplyX =
+                        groupAddition.monoid.semigroup yLeftMultiplyX zLeftMultiplyX
+
+                    xPlusYRightMultiplyZ =
+                        Ring.numberRing.multiplication.semigroup
+                            (groupAddition.monoid.semigroup x y)
+                            z
+
+                    xRightMultiplyZ =
+                        Ring.numberRing.multiplication.semigroup x z
+
+                    yRightMultiplyZ =
+                        Ring.numberRing.multiplication.semigroup y z
+
+                    xRightMultiplyZPlusYRightMultiplyZ =
+                        groupAddition.monoid.semigroup xRightMultiplyZ yRightMultiplyZ
+                in
+                Expect.true "All equal a"
+                    (xLeftMultiplyYplusZ
+                        == yLeftMultiplyXPlusZLeftMultiplyX
+                    )
         ]
